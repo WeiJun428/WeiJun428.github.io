dec43f3043da641f6b2b01ebafcbf78b378c2fa3
Wei Jun Tan <wj428@uw.edu>
2023-07-28 22:09:16 -0700

    Setup React repo with eslint, prettier, stylelint, and initial MUI components

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
+
+function About() {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h1" gutterBottom>
+          Wei Jun Tan
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default About;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppBar from "../NavBar/NavBar";
+import About from "../About/About";
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar />
+      <About />
+    </div>
+  );
+}
+
+export default App;

d79a5bdca50138e19b48b52c2d6c194b7db76d22
Wei Jun Tan <wj428@uw.edu>
2023-08-07 16:13:39 -0700

    Enable light/dark mode for future toggle

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import theme from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme("light")}>
       <CssBaseline />
       <App />
     </ThemeProvider>
--- src/theme.js
+++ src/theme.js
@@ -2,50 +2,55 @@
 import { createTheme } from "@mui/material/styles";
 
 // A custom theme for this app
-const theme = createTheme({
-  palette: {
-    bgcolor: "rgba(249, 250, 251, 0.7)",
-  },
-  typography: {
-    h1: {
-      fontFamily: ["Playfair Display"],
-      fontWeight: 800,
-      fontSize: "3rem",
+const theme = (mode) => {
+  return createTheme({
+    palette: {
+      mode,
+      background: {
+        default: mode === "light" ? "rgba(249, 250, 251, 0.7)" : "#000",
+      },
     },
-    h2: {
-      fontFamily: ["Playfair Display"],
-      fontWeight: 800,
-      fontSize: "1rem",
+    typography: {
+      h1: {
+        fontFamily: ["Playfair Display"],
+        fontWeight: 800,
+        fontSize: "3rem",
+      },
+      h2: {
+        fontFamily: ["Playfair Display"],
+        fontWeight: 800,
+        fontSize: "1rem",
+      },
+      h3: {
+        fontFamily: ["Playfair Display"],
+        fontWeight: 800,
+      },
+      h4: {
+        fontFamily: ["Playfair Display"],
+        fontWeight: 800,
+      },
+      h5: {
+        fontFamily: ["Playfair Display"],
+        fontWeight: 800,
+      },
+      h6: {
+        fontFamily: ["Playfair Display"],
+        fontWeight: 800,
+      },
+      fontFamily: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
     },
-    h3: {
-      fontFamily: ["Playfair Display"],
-      fontWeight: 800,
-    },
-    h4: {
-      fontFamily: ["Playfair Display"],
-      fontWeight: 800,
-    },
-    h5: {
-      fontFamily: ["Playfair Display"],
-      fontWeight: 800,
-    },
-    h6: {
-      fontFamily: ["Playfair Display"],
-      fontWeight: 800,
-    },
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
-});
+  });
+};
 
 export default theme;

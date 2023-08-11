e1f29ade5084f3683b12769c1415425ef6e2089a
Wei Jun Tan <wj428@uw.edu>
2023-08-11 08:52:39 -0700

    Add responsive font size

--- src/theme.js
+++ src/theme.js
@@ -1,59 +1,61 @@
 import { red } from "@mui/material/colors";
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 // A custom theme for this app
 const theme = (mode) => {
-  return createTheme({
-    palette: {
-      mode,
-      background: {
-        default: mode === "light" ? "rgba(249, 250, 251, 0.7)" : "#000",
+  return responsiveFontSizes(
+    createTheme({
+      palette: {
+        mode,
+        background: {
+          default: mode === "light" ? "rgba(249, 250, 251, 0.7)" : "#000",
+        },
+        primary: {
+          main: mode === "light" ? red[700] : red[300],
+        },
       },
-      primary: {
-        main: mode === "light" ? red[700] : red[300],
+      typography: {
+        h1: {
+          fontFamily: ["Playfair Display"],
+          fontWeight: 800,
+          fontSize: "3rem",
+        },
+        h2: {
+          fontFamily: ["Playfair Display"],
+          fontWeight: 800,
+          fontSize: "1rem",
+        },
+        h3: {
+          fontFamily: ["Playfair Display"],
+          fontWeight: 800,
+        },
+        h4: {
+          fontFamily: ["Playfair Display"],
+          fontWeight: 800,
+        },
+        h5: {
+          fontFamily: ["Playfair Display"],
+          fontWeight: 800,
+        },
+        h6: {
+          fontFamily: ["Playfair Display"],
+          fontWeight: 800,
+        },
+        fontFamily: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ].join(","),
       },
-    },
-    typography: {
-      h1: {
-        fontFamily: ["Playfair Display"],
-        fontWeight: 800,
-        fontSize: "3rem",
-      },
-      h2: {
-        fontFamily: ["Playfair Display"],
-        fontWeight: 800,
-        fontSize: "1rem",
-      },
-      h3: {
-        fontFamily: ["Playfair Display"],
-        fontWeight: 800,
-      },
-      h4: {
-        fontFamily: ["Playfair Display"],
-        fontWeight: 800,
-      },
-      h5: {
-        fontFamily: ["Playfair Display"],
-        fontWeight: 800,
-      },
-      h6: {
-        fontFamily: ["Playfair Display"],
-        fontWeight: 800,
-      },
-      fontFamily: [
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Roboto",
-        '"Helvetica Neue"',
-        "Arial",
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(","),
-    },
-  });
+    }),
+  );
 };
 
 export default theme;

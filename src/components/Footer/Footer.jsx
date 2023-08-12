b1c4e84420d08ba129ffca0c962e05ab64bc626c
Wei Jun Tan <wj428@uw.edu>
2023-08-11 21:57:42 -0700

    Add hint in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-import { Typography } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import { Box, Typography } from "@mui/material";
 
 function Footer() {
   return (
-    <Typography variant="body1" component="footer" color="text.secondary">
-      Designed by Wei Jun Tan. Built with React CRA and Material UI.
-    </Typography>
+    <Box component="footer">
+      <Typography variant="body1" color="text.secondary">
+        Designed by Wei Jun Tan. Built with React CRA and Material UI.
+      </Typography>
+      <Typography variant="body1" color="text.secondary">
+        For fun: Find the hidden{" "}
+        <Brightness4Icon fontSize="small" color="primary" /> to switch off the
+        light!
+      </Typography>
+    </Box>
   );
 }
 

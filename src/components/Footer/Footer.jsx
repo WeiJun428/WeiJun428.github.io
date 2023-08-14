466da73bacf2ed32b775f572064cd03c899ed9bf
Wei Jun Tan <wj428@uw.edu>
2023-08-14 10:26:48 -0700

    Show the ThemeToggleButton

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 
 function Footer() {
   return (
-    <Box component="footer">
-      <Typography variant="body1" color="text.secondary">
-        Designed by Wei Jun Tan. Built with React CRA and Material UI.
-      </Typography>
-      <Typography variant="body1" color="text.secondary">
-        For fun: Find the hidden{" "}
-        <Brightness4Icon fontSize="small" color="primary" /> to switch off the
-        light!
-      </Typography>
-    </Box>
+    <Typography variant="body1" color="text.secondary" component="footer">
+      Designed by Wei Jun Tan. Built with React CRA and Material UI.
+    </Typography>
   );
 }
 
--- src/components/Theme/ThemeToggleButton.jsx
+++ src/components/Theme/ThemeToggleButton.jsx
@@ -2,67 +2,29 @@ import React from "react";
 
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { Box, Fade, IconButton, useTheme } from "@mui/material";
+import { IconButton, useTheme } from "@mui/material";
 
 import ColorModeContext from "./ThemeModeContext";
-import useHover from "../../hooks/useHover";
 
 function ThemeToggleButton() {
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
-  const timeOutId = React.useRef(null);
-  const [isVisible, setIsVisble] = React.useState(false);
-  const {
-    isHovered,
-    handleMouseEnter,
-    handleMouseLeave,
-    handleTouchStart,
-    handleTouchEnd,
-  } = useHover();
-
-  React.useEffect(() => {
-    if (isHovered) {
-      if (timeOutId.current) {
-        clearTimeout(timeOutId.current);
-      }
-      timeOutId.current = setTimeout(() => {
-        setIsVisble(true);
-      }, 200);
-    } else {
-      if (timeOutId.current) {
-        clearTimeout(timeOutId.current);
-      }
-      if (isVisible) {
-        setIsVisble(false);
-      }
-    }
-  }, [isHovered, isVisible, setIsVisble]);
 
   return (
-    <Box
-      ariaLabel="hidden theme toggle"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
+    <IconButton
+      onClick={colorMode.toggleThemeMode}
+      sx={{
+        "&:hover": {
+          color: "primary.main",
+        },
+      }}
     >
-      <Fade in={isVisible}>
-        <IconButton
-          onClick={colorMode.toggleThemeMode}
-          sx={{
-            "&:hover": {
-              color: "primary.main",
-            },
-          }}
-        >
-          {theme.palette.mode === "dark" ? (
-            <Brightness7Icon />
-          ) : (
-            <Brightness4Icon />
-          )}
-        </IconButton>
-      </Fade>
-    </Box>
+      {theme.palette.mode === "dark" ? (
+        <Brightness7Icon />
+      ) : (
+        <Brightness4Icon />
+      )}
+    </IconButton>
   );
 }
 

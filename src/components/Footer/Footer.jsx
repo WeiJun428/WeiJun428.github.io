fe4f2e5319eaee90f5c256e15cbcc36a353a360f
Wei Jun Tan <wj428@uw.edu>
2023-08-17 10:11:55 -0700

    Add padding to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,12 @@ import { Typography } from "@mui/material";
 
 function Footer() {
   return (
-    <Typography variant="body1" color="text.secondary" component="footer">
+    <Typography
+      variant="body1"
+      color="text.secondary"
+      component="footer"
+      sx={{ pl: 2 }}
+    >
       Designed by Wei Jun Tan. Built with React and MUI. &copy; 2023
     </Typography>
   );

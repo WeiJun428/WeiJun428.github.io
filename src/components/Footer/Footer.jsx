db9afccf1afc6348f298b789e0e7daea0f43afe1
Wei Jun Tan <wj428@uw.edu>
2023-08-17 10:07:42 -0700

    Modify footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Typography } from "@mui/material";
 function Footer() {
   return (
     <Typography variant="body1" color="text.secondary" component="footer">
-      Designed by Wei Jun Tan. Built with React CRA and Material UI.
+      Designed by Wei Jun Tan. Built with React and MUI. &copy; 2023
     </Typography>
   );
 }

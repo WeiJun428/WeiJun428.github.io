6801c34c7b9960d29154e720173b78ab935a2011
Wei Jun Tan <wj428@uw.edu>
2024-04-18 09:46:04 -0700

    Add tooltip to header icon buttons

--- src/components/Header/Contact.jsx
+++ src/components/Header/Contact.jsx
@@ -4,7 +4,7 @@ import ArticleIcon from "@mui/icons-material/Article";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import MailIcon from "@mui/icons-material/Mail";
-import { IconButton, ButtonGroup, Link } from "@mui/material";
+import { IconButton, ButtonGroup, Link, Tooltip } from "@mui/material";
 
 import ThemeToggleButton from "../Theme/ThemeToggleButton";
 
@@ -12,18 +12,22 @@ const data = [
   {
     link: "mailto:wj428@uw.edu",
     icon: <MailIcon />,
+    label: "Mail",
   },
   {
     link: "https://www.linkedin.com/in/wei-jun-tan/",
     icon: <LinkedInIcon />,
+    label: "LinkedIn",
   },
   {
     link: "https://github.com/WeiJun428",
     icon: <GitHubIcon />,
+    label: "GitHub",
   },
   {
     link: "/docs/resume.pdf",
     icon: <ArticleIcon />,
+    label: "Resume",
   },
 ];
 
@@ -32,21 +36,22 @@ function Contact() {
     <ButtonGroup variant="outlined">
       {data.map((d) => {
         return (
-          <Link
-            href={d.link}
-            key={d.link}
-            target={d.link.startsWith("https") ? "_blank" : "_self"}
-          >
-            <IconButton
-              sx={{
-                "&:hover": {
-                  color: "primary.main",
-                },
-              }}
+          <Tooltip title={d.label} key={d.link}>
+            <Link
+              href={d.link}
+              target={d.link.startsWith("https") ? "_blank" : "_self"}
             >
-              {d.icon}
-            </IconButton>
-          </Link>
+              <IconButton
+                sx={{
+                  "&:hover": {
+                    color: "primary.main",
+                  },
+                }}
+              >
+                {d.icon}
+              </IconButton>
+            </Link>
+          </Tooltip>
         );
       })}
       <ThemeToggleButton />
--- src/components/Theme/ThemeToggleButton.jsx
+++ src/components/Theme/ThemeToggleButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { IconButton, useTheme } from "@mui/material";
+import { IconButton, Tooltip, useTheme } from "@mui/material";
 
 import ColorModeContext from "./ThemeModeContext";
 
@@ -10,21 +10,21 @@ function ThemeToggleButton() {
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
 
+  const isDark = theme.palette.mode === "dark";
+
   return (
-    <IconButton
-      onClick={colorMode.toggleThemeMode}
-      sx={{
-        "&:hover": {
-          color: "primary.main",
-        },
-      }}
-    >
-      {theme.palette.mode === "dark" ? (
-        <Brightness7Icon />
-      ) : (
-        <Brightness4Icon />
-      )}
-    </IconButton>
+    <Tooltip title={isDark ? "Light Mode" : "Dark Mode"}>
+      <IconButton
+        onClick={colorMode.toggleThemeMode}
+        sx={{
+          "&:hover": {
+            color: "primary.main",
+          },
+        }}
+      >
+        {isDark ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+    </Tooltip>
   );
 }
 
